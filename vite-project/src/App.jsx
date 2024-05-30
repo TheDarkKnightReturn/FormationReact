209ba3b80d935627d9cd510c0229f6ec8f967749
kcomont <kcomont@horizontalsoftware.com>
2024-05-30 14:01:43 +0200

    30052024

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,26 @@
-import styles from "./App.module.css";
+import { Card } from "./components/Card";
+import { Button } from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  return (
-    <p className={styles.text}>
-      hello World
-    </p>
-  )
-}
+  const [counter, setCounter] = useState[0];
 
+  const increment = () =>  {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="m-7 text-3xl font-bold underline">Hello world!</h1>
+        <Card title="Title Props 2" subTitle="subtitle Props 2"></Card>
+        <Card title="Title Props without subtitle"></Card>
+        <Button title="Cliquer" onCardClick={increment}>
+           counter : {counter}
+        </Button>
+      </div>
+    </div>
+  );
+}
 
 export default App;
---
+++ vite-project/src/components/Card.jsx
@@ -0,0 +1,29 @@
+import * as PT from "prop-types";
+
+const Card = ({title, subTitle}) => (
+  <div className="m-5 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col items-center pb-10 m-20">
+      <img
+        className="w-24 h-24 mb-3 rounded-full shadow-lg"
+        src="/public/vite.svg"
+        alt="Bonnie image"
+      />
+      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <span className="text-sm text-gray-500 dark:text-gray-400">
+        {subTitle}
+      </span>
+    </div>
+  </div>
+);
+
+Card.propTypes = {
+    title: PT.string.isRequired,
+    subTitle: PT.node
+}
+
+export { Card };
+
+//permet d'exporter un seul composant par défaut
+//export default Card;
